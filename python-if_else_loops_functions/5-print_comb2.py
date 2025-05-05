1d700568b7d4ba2f01d2f0951b4817a579987b03
Alurae <10475@holbertonstudents.com>
2025-05-05 19:44:02 +0200

    Refactor print statements in 5-print_comb2.py for consistency

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
-# This program prints numbers from 00 to 99 with leading zeros,
-# separated by ', ', using only one loop and at most two print functions.
-
 for i in range(100):
     if i < 99:
-        print(f"{i:02}, ", end="")
+        print("{:02}, ".format(i), end="")
     else:
-        print(f"{i:02}")
+        print("{:02}".format(i))
