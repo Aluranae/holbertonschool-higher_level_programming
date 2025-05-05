8a2defe1b4680b75f9e9f048302082775f158e81
Alurae <10475@holbertonstudents.com>
2025-05-05 19:32:40 +0200

    Refactor print logic in 3-print_alphabt.py for clarity

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-# This program prints the lowercase ASCII alphabet except 'e' and 'q',
-# using one loop and one print function, without variables or imports.
-
 for i in range(97, 123):
     if i == ord('e') or i == ord('q'):
         continue
-    print(chr(i), end="")
+    else:
+        print(chr(i), end="")
