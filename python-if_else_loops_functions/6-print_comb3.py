fd9980d4245624b4ca4159041f8d1db26fcee6ee
Alurae <10475@holbertonstudents.com>
2025-05-05 19:47:36 +0200

    Refactor print statements in 6-print_comb3.py for consistency

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-# This program prints all different combinations of two digits,
-# without repetition and in ascending order,
-# using at most two loops and three print functions.
-
-
 for i in range(10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i, j), end="")
         else:
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
