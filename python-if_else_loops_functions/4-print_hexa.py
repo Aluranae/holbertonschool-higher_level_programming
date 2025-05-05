01c2fb4f4242dba60f585627ff2df5c21e539ca1
Alurae <10475@holbertonstudents.com>
2025-05-05 19:40:54 +0200

    Refactor print statement in 4-print_hexa.py for consistency

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# This program prints all numbers from 0 to 98 in decimal and hexadecimal,
-# using one loop and one print function, without variables or imports.
-
 for i in range(0, 99):
-    print(f"{i} = {hex(i)}")
+    print("{} = {}".format(i, hex(i)))
