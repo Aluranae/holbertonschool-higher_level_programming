f0ef9bb3db89ff3d630b63771f3deacf8de6817b
Aluranae <10475@holbertonstudents.com>
2025-05-06 11:56:13 +0200

    Add docstring to add function in add_0.py for improved documentation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 
 from add_0 import add
-"""My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-
 
 if __name__ == "__main__":
 
