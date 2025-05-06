2a21ee375676ab4852a96faac5b73fb70587c816
Aluranae <10475@holbertonstudents.com>
2025-05-06 14:04:54 +0200

    Remove unnecessary docstring and clean up whitespace in 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Afficher le résultat de 4 opérations mathématiques avec a=10 et b=5
-en important les fonctions depuis calculator_1"""
-
 from calculator_1 import add, sub, mul, div
 
 
 if __name__ == "__main__":
-
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
 
     print("{} - {} = {}".format(a, b, sub(a, b)))
