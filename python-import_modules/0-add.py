74f28d0e659c42a0dc8d3e919d82c420d67dfa19
Aluranae <10475@holbertonstudents.com>
2025-05-06 11:09:45 +0200

    Add initial implementations for addition and related modules

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Afficher le résultat de l'addition de 1 et 2 en utilisant la fonction add du fichier add_0"""
+
+from add_0 import add
+
+
+if __name__ == "__main__":
+
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print(" {} + {} = {}".format(a, b, result))
