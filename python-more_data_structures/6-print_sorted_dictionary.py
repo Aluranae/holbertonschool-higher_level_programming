8f213222dce6f2372f91d74afc92b9c671b3b601
Alurae <10475@holbertonstudents.com>
2025-05-12 20:19:45 +0200

    Simplify print_sorted_dictionary function by removing unnecessary variables

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary)
-    for key in sorted_keys:
-        value = a_dictionary[key]
-        print("{}:{}".format(key, value))
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
