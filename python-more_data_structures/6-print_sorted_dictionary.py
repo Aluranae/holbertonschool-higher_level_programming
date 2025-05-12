7c84946bcefd79f653824499aead0b5778a30b1f
Alurae <10475@holbertonstudents.com>
2025-05-12 20:14:27 +0200

    Refactor variable name in print_sorted_dictionary function for clarity

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary)
-    for keys in sorted_keys:
-        value = a_dictionary[keys]
-        print("{}:{}".format(keys, value))
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        print("{}:{}".format(key, value))
