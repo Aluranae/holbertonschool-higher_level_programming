9527c8dfb0a64b52023378dee2dce17d1561e371
Alurae <10475@holbertonstudents.com>
2025-05-12 23:40:29 +0200

    Fix initial max_value in best_score function to None for proper comparison

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_key = 0
-    max_value = 0
+    max_value = None
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
