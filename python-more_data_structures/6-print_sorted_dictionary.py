f186bed6fe32d71ef2747cef1c19fa86423f39a6
Alurae <10475@holbertonstudents.com>
2025-05-12 20:05:56 +0200

    Fix print format in print_sorted_dictionary function for consistent output

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary)
     for keys in sorted_keys:
         value = a_dictionary[keys]
-        print(keys, ":", value)
+        print("{}:{}".format(keys, value))
