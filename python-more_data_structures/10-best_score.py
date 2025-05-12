4c7f8fa217f1f5859d87af6790608dd3703740fd
Alurae <10475@holbertonstudents.com>
2025-05-12 23:42:06 +0200

    Fix variable initialization order in best_score function for clarity

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,8 +3,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_key = 0
-    max_value = None
+    max_value = 0
+    max_key = None
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
