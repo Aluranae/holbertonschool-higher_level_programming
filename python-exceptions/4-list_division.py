2d73eccaa02e78e5e106b80682b89fd038e69eb0
Aluranae <10475@holbertonstudents.com>
2025-05-13 12:56:52 +0200

    Refactor list_division function to improve structure and readability

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
+    for i in range(list_length):
+        try:
+            num = my_list_1[i]
+            den = my_list_2[i]
+            result = num / den
+            result_list.append(result)
+        except (TypeError, ValueError):
+            print("wrong type")
+            result_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result_list.append(0)
+        except IndexError:
+            print("out of range")
+            result_list.append(0)
+        finally:
+            pass
+    return result_list
