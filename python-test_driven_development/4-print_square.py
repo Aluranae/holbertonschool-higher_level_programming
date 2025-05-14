5974ab9f1712b87e4093d42c4d1e14c8535d0e54
Alurae <10475@holbertonstudents.com>
2025-05-14 21:17:19 +0200

    Refactor print_square function to use a single print statement for each row

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,4 +15,6 @@ Raises TypeError or ValueError depending on input."""
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
-        print("#" * size)
+        for j in range(size):
+            print("#", end="")
+        print()
