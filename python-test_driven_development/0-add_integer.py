44fb01ef4238acf052d316c26c278e51d95c90ee
Aluranae <10475@holbertonstudents.com>
2025-05-14 15:32:24 +0200

    Refactor type checks in add_integer function for improved readability

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,8 @@ def add_integer(a, b=98):
     """Adds two integers or floats.
 Casts floats to integers and returns the sum.
 Raises TypeError if inputs are not valid."""
-
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
