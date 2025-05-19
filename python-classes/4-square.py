5f1e48cc15bdf4d93a8fac7416b88c9c7d957fc3
Alurae <10475@holbertonstudents.com>
2025-05-19 20:41:25 +0200

    Fix TypeError message in Square class size setter for consistency

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -42,7 +42,7 @@ class Square:
             ValueError: If value is negative.
         """
         if not isinstance(value, int):
-            raise TypeError("value must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("value must be >= 0")
         self.__size = value
